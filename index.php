d87db1748d15e80e2537040fde1981a6368547ba
LOSAFABIO <fabio.losa95@gmail.com>
2022-02-14 17:06:20 +0100

    Ok testo ma non censura

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+    $testo = "Lorem ipsum dolor sit amet consectetur, error , magni! Adipisicing elit, error , magni!";
+
+    $lunghezzaTesto = strlen($testo);
+
+    $parolaCensurata = $_GET["error"];
+
+    $censura = "***";
+    
+    $testo =  str_replace($parolaCensurata, $censura, $testo); 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+        
+    <h3>
+        Lunghezza del testo : 
+        <?php echo $lunghezzaTesto?>        
+        parole
+    </h3>
+    <h1>
+        <?php echo $testo ?>        
+    </h1>
+
+</body>
+</html>
